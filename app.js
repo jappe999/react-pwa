1666d620ad2e676f8696dae42048361e655366e4
Jasper van der Linden <jappe@pop-os.localdomain>
2018-11-04 18:54:36 +0100

    fix: Add rendering engine

--- app.js
+++ app.js
@@ -15,11 +15,16 @@ app.use((req, res, next) => {
   next()
 })
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
+
+
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'client/ypublic')))
 
 app.use('/', indexRouter)
 app.use('/api/blogs', blogsRouter)
